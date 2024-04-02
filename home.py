7566067e389cac6f9060dd11ee260ff98b3d3fc2
Hossam2ELWWY <149169850+Hossam2ELWWY@users.noreply.github.com>
2024-04-02 15:29:35 +0200

    Add files via upload

---
+++ home.py
@@ -0,0 +1,55 @@
+import pandas as pd
+import numpy as np
+import plotly.express as px
+import streamlit as st
+from PIL import Image
+from ydata_profiling import ProfileReport
+from streamlit_pandas_profiling import st_profile_report 
+df = pd.read_csv(r"C:\Users\Maydoum\Downloads\Compressed\apartments_pl_2024_01.csv")
+df.drop(columns=['condition' , 'id' , 'buildingMaterial' , 'collegeDistance' , 'restaurantDistance' , 'longitude', 'latitude'], inplace=True)
+df.dropna(subset=[ 'schoolDistance' , 'hasElevator','pharmacyDistance',
+                  'kindergartenDistance', 'postOfficeDistance' , 'floorCount' , 'clinicDistance',  
+                   'poiCount']
+          , inplace=True)
+df['type'].fillna(df['type'].mode()[0] , inplace=True)
+df['floor'].fillna(df['floor'].mode()[0] ,inplace = True)
+df['floorCount'].fillna(df['floorCount'].mode()[0] ,inplace = True)
+df['buildYear'].fillna(df['buildYear'].mode()[0] , inplace=True)
+st.title('Welcome to the Home Page 🤓')
+
+st.image(r"city-paris-france-building-wallpaper-preview.jpg" , width=900)
+st.snow()
+st.subheader('This EDA App is for appartments in poland' )
+st.write("<span style='color: red; font-family: Arial, sans-serif; font-size: 20px;'>Data link <a href='https://www.kaggle.com/datasets/krzysztofjamroz/apartment-prices-in-poland/data'>here</a></span>", unsafe_allow_html=True)
+sample = st.checkbox("show sample of data")  
+if sample :
+    st.write(df.sample())
+head = st.checkbox("show head of data ")
+if head :
+    st.write(df.head())    
+
+
+
+# Display information about the dataset
+st.write("<span style='font-size: 24px;color: red;'>About Dataset:</span>", unsafe_allow_html=True)
+st.write("The dataset contains apartment sales and rent offers from the 15 largest cities in Poland:")
+st.write("- Warsaw")
+st.write("- Lodz")
+st.write("- Krakow")
+st.write("- Wroclaw")
+st.write("- Poznan")
+st.write("- Gdansk")
+st.write("- Szczecin")
+st.write("- Bydgoszcz")
+st.write("- Lublin")
+st.write("- Katowice")
+st.write("- Bialystok")
+st.write("- Czestochowa")
+st.write("The data comes from local websites with apartments for sale.")
+st.write("To fully capture the neighborhood of each apartment better, each offer was extended by data from the Open Street Map with distances to points of interest (POI).")
+st.write("The data is collected monthly and covers a timespan between August 2023 and March 2024.")
+
+if st.button('Press to show Report about Dataset'):
+    pr = ProfileReport(df, explorative=True)
+    st.header('**Pandas Profiling Report**')
+    st_profile_report(pr)
---
+++ overview.py
@@ -0,0 +1,97 @@
+import pandas as pd
+import numpy as np
+import plotly.express as px
+import streamlit as st
+from PIL import Image
+from ydata_profiling import ProfileReport
+from streamlit_pandas_profiling import st_profile_report 
+df = pd.read_csv(r"C:\Users\Maydoum\Downloads\Compressed\apartments_pl_2024_01.csv")
+df.drop(columns=['condition' , 'id' , 'buildingMaterial' , 'collegeDistance' , 'restaurantDistance'], inplace=True)
+st.title('Welcome to the Overview Page 📈  ')
+
+
+st.header('average price appartments in Poland'  )
+st.plotly_chart( px.box(df , x = 'price' , template= 'plotly_dark' ,color_discrete_sequence=px.colors.sequential.Electric_r )
+,use_container_width=True) 
+
+
+
+
+c1,c33,c2 = st.columns([4,1,4])
+
+
+with c1 :
+    st.title('Square meters vs price ')
+    st.plotly_chart(px.scatter(df , x = 'squareMeters' , y = 'price' , template= 'plotly_dark' ,
+                               color_discrete_sequence=px.colors.sequential.Electric_r ,width=600,height=400 ))
+    st.markdown('<p style="font-size:24px; color:orange;">More square meters means more price.</p>', unsafe_allow_html=True)
+
+with c2:
+
+    st.title('City vs price ')
+    st.plotly_chart(px.bar( df.groupby('city')['price'].mean().reset_index(), x= 'price' , y = 'city' , template= 'plotly_dark' ,
+       color_discrete_sequence=px.colors.sequential.Electric_r , color='price'
+       ,width=600,height=400))
+    #smumarry chart as text
+    st.markdown('<p style="font-size:24px; color:orange;">The highest average real estate in the city in Poland is located in Warszawa.</p>', unsafe_allow_html=True)
+c1,c33,c2 = st.columns([4,1,4]) 
+with c1 : 
+    st.title('Rooms vs price ')
+    st.plotly_chart(px.bar(df.groupby('rooms')['price'].median().reset_index() , x = 'rooms' , y= 'price' , template= 'plotly_dark' ,
+       color_discrete_sequence=px.colors.sequential.Electric_r ,color='rooms', title='Average apartment prices for rooms'))
+    st.markdown('<p style="font-size:24px; color:orange;">More rooms means more price.</p>', unsafe_allow_html=True)
+with c2 :
+    st.title('Floor vs price ')
+    st.plotly_chart(px.line(df.groupby('floor')['price'].sum().reset_index().sort_values(by='floor') , x= 'floor' , y= 'price' , template= 'plotly_dark' ,
+        color_discrete_sequence=px.colors.sequential.Electric_r , title= 'Total price VS floor' ))
+    st.markdown('<p style="font-size:24px; color:orange;">The highest price is in the ground floor.</p>', unsafe_allow_html=True)
+c1,c33,c2 = st.columns([4,1,4])
+with c1 :
+    st.title('Building year vs price ')
+    bins = [1850 ,1900, 2000, 2024]  
+    labels = ['old', 'medium', 'modren']
+    df['distance_year_group'] = pd.cut(df['buildYear'], bins=bins, labels=labels)
+    st.plotly_chart(px.bar(df.groupby('distance_year_group')['price'].sum().reset_index().sort_values(by='price')
+     , x='distance_year_group' , y = 'price' , template= 'plotly_dark' 
+     ,color_discrete_sequence=px.colors.sequential.Electric_r ,
+     color='distance_year_group'))
+    st.markdown('<p style="font-size:24px; color:orange;">The highest price is in the modern age.</p>', unsafe_allow_html=True)
+    
+with c2 : 
+    st.title('ownership Vs Price ')
+    st.plotly_chart(px.bar(df.groupby('ownership')['price'].sum().reset_index().sort_values(by='price')
+     , x='ownership' , y = 'price' , template= 'plotly_dark' 
+     ,color_discrete_sequence=px.colors.sequential.Electric_r ,
+     color='ownership'))
+    st.markdown('<p style="font-size:24px; color:orange;">The highest price is in the condominium ownership.</p>', unsafe_allow_html=True)
+
+c1,c33,c2 = st.columns([4,1,4])
+with c1 : 
+    bins = [0,0.4,0.9,2.56]  
+    labels = ['Near', 'Medium', 'Far']
+    df['distance_clinic_group'] = pd.cut(df['clinicDistance'], bins=bins, labels=labels)
+
+            
+    st.title("Rooms Vs price Vs distance_clinic ")
+    st.plotly_chart(px.bar(df.groupby(['rooms' , 'distance_clinic_group'])['price'].mean().reset_index().sort_values(by='price') ,
+    x = 'rooms' , y = 'price' , color='distance_clinic_group' , color_discrete_sequence=px.colors.sequential.Electric_r , title= 'price VS rooms VS distance_clinic' , 
+     barmode='group' , template='plotly_dark' ))
+    st.markdown('<p style="font-size:24px; color:orange;">The highest price is in the nearest distance and in the most rooms.</p>', unsafe_allow_html=True)
+
+with c2 :
+    st.title("Rooms Vs price Vs Security ")
+    grouped_df = df.groupby(['rooms', 'hasSecurity'])['price'].mean().reset_index()
+    st.plotly_chart(
+px.line(grouped_df, x='rooms', y='price', color='hasSecurity',
+             labels={'rooms': 'Number of Rooms', 'price': 'Average Price (PLN)', 'hasSecurity': 'has Security'},
+             title='Average Price vs Number of Rooms by Security', template='plotly_dark',color_discrete_sequence=px.colors.sequential.Electric_r ) 
+
+)    
+    st.markdown('<p style="font-size:24px; color:orange;">The highest price is the most rooms and has security .</p>', unsafe_allow_html=True)
+df['squareMeters_cat'] = pd.cut(df['squareMeters'] , bins = [0 , 50 , 100 ,150] , labels = ["<50" , "50-100" , ">100"])
+st.header('squareMeters_cat Vs price Vs rooms') 
+st.plotly_chart(px.box(df, x='squareMeters_cat', y='price', color='rooms',
+             labels={'squareMeters_cat': 'Square Meters Category', 'price': 'Price (PLN)', 'rooms': 'Number of Rooms'},
+             title='Price Distribution by Square Meters Category and Number of Rooms' , template='plotly_dark')
+,use_container_width=True)   
+st.markdown('<p style="font-size:24px; color:orange;">The highest price is in the most rooms and in the most square meters.</p>', unsafe_allow_html=True)
